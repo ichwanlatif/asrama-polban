79c84b7b182f7b3bad5099fa89efadc30432c7c3
Chiibee <ichwanlatiffajari@gmail.com>
2021-04-27 01:11:44 +0700

    fix conflict dev

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -2,9 +2,12 @@ import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 import {HashRouter, Route } from "react-router-dom";
 import Main from "../Router";
-import "../css/app.min.css";
+// import "../css/app.min.css";
 import Sidebar from "./Navigation/Sidebar";
 import Navbar from "./Navigation/Navbar";
+import "../vendor/adminbootstrap/css/sb-admin-2.min.css";
+import "../vendor/adminbootstrap/js/sb-admin-2.min.js";
+import "../vendor/fontawesome/css/all.min.css";
 
 class App extends Component {
   constructor(){
